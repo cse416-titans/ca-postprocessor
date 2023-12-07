be7b5c9ab57d8c18c25d6941cff266c9d9bc6c17
Donghyun Lee <ufg1162@naver.com>
2023-12-07 15:53:40 -0500

    Update
    DB upload for district plans have been modified

--- pathUtil.py
+++ pathUtil.py
@@ -9,3 +9,10 @@ def getPathName(path):
 def getClusterSetPathName(path):
     pathName = "/".join(path.replace("\\", "/").split("/")[1:4])
     return pathName
+
+def getPlanPathName(path):
+    pathArr = path.replace("\\", "/").split("/")[1:]
+    pathName = pathArr[0:2]
+    pathName.append(pathArr[-1])
+    pathName[-1] = pathName[-1].split(".")[0]
+    return pathName
